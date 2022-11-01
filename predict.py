24a8e797399d9f1c2140976e52416d4444e439cb
ariel <ariel415el@gmail.com>
2022-11-01 22:02:46 +0200

    fit cog to a100. Change mp4 formating. load models once

--- predict.py
+++ predict.py
@@ -2,40 +2,63 @@ import argparse
 import subprocess
 import torch
 import numpy as np
-from cog import BasePredictor, Path, Input
-from scripts.inf_zoom import write_frames, zoom_in, inf_inpaint, text2img
+from scripts.inf_zoom import write_frames, zoom_in, text2img, self_inpaint, OmegaConf, load_model_from_config, PLMSSampler, instantiate_from_config, DDIMSampler
+from cog import BasePredictor, Path, Input, BaseModel
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+class Output(BaseModel):
+    mp4: Path = None
+    gif: Path = None
 
 
 class Predictor(BasePredictor):
     def setup(self):
         subprocess.run(["pip3", "install", "-e", "."])
 
+        txt2img_conf = 'configs/stable-diffusion/v1-inference.yaml'
+        txt2img_ckpt = 'models--runwayml--stable-diffusion-v1-5/snapshots/51b78538d58bd5526f1cf8e7c03c36e2799e0178/v1-5-pruned-emaonly.ckpt'
+        config = OmegaConf.load(txt2img_conf)
+        txt2img_model = load_model_from_config(config, txt2img_ckpt).to(device)
+        self.txt2img_sampler = PLMSSampler(txt2img_model)
+
+        inpaint_conf = 'configs/stable-diffusion/v1-inpainting-inference.yaml'
+        inpaint_ckpt = 'models--runwayml--stable-diffusion-inpainting/snapshots/e5fae204b8239db36f2cdc2c8b773d10c457a567/sd-v1-5-inpainting.ckpt'
+        config = OmegaConf.load(inpaint_conf)
+        inpaint_model = instantiate_from_config(config.model)
+        inpaint_model.load_state_dict(torch.load(inpaint_ckpt)["state_dict"], strict=False)
+        inpaint_model = inpaint_model.to(device)
+        self.inpaint_sampler = DDIMSampler(inpaint_model)
+
     def predict(
             self,
             prompt: str = Input(description="Prompt"),
+            output_format: str = Input(description="infinite loop gif or mp4 video", default="mp4", choices=["mp4", "gif"]),
+            inpaint_iter: int = Input(description="Number of iterations of pasting the image in it's center and inpainting the boarders", default=2),
 
-    ) -> Path:
+    ) -> Output:
         opt = argparse.Namespace()
-        opt.H = opt.W = 512
-        opt.f = 8
-        opt.C = 4
+        opt.dim = 512
+        opt.zoom_factor = 4
+        opt.inpaint_iter = int(inpaint_iter)
         opt.scale = 7.5
         opt.ddim_steps = 50
         opt.ddim_eta = 0
         opt.prompt = prompt
 
-        output_path = "infinite_zoom.gif"
+        img = text2img(self.txt2img_sampler, opt)
+        img = self_inpaint(self.inpaint_sampler, img, opt)
+        frames = zoom_in(np.array(img)[..., ::-1], factor=opt.zoom_factor, n_self_paste=3, n_frames=60)
 
-        img = text2img(opt)
-        img, mask = inf_inpaint(img, opt)
-        img, mask = inf_inpaint(img, opt)
-        frames = zoom_in(np.array(img)[..., ::-1], n_frames=60)
+        output_format = str(output_format)
+        ext = output_format.split(".")[-1]
+        fname = f"infinit_zoom.{ext}"
+        write_frames(frames, fname=fname, fps=30)
 
-        write_frames(frames, fname=output_path, fps=30)
-
-        return Path(output_path)
-
+        if ext == 'gif':
+            return Output(gif=Path(fname))
+        else:
+            return Output(mp4=Path(fname))
 
 
